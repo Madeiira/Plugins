faeec93d5d00cc9edc5f84797bdf6e54a561d506
Gabriel <gmmanjos@gmail.com>
2020-08-17 10:14:19 -0300

    -First plugin

---
+++ wp-Plugins.php
@@ -0,0 +1,33 @@
+<?php 
+
+/*
+Plugin Name: Plugins 
+Plugin URI: 
+Description: 
+Version: 1.0
+Author: Gabriel Madeira
+Author URI: https://github.com/Madeiira
+Text Domain: hW
+
+*/
+
+/* function wp_alert_wp_Hello_World(){
+
+
+echo '<script> alert("Sou Fodas"); </script>';
+}
+
+
+add_action( 'login_head','wp_alert_wp_Hello_World' ); // especificar aonde você vai querer a ação
+*/
+//add_action();  // quando // carregamento // eventos // function
+//add_filter(); // aonde  //              //         //  function
+
+
+//testing advertising in content() // substitui todo o content do meu wp 
+/*function add_advertising(){
+ echo"<h1 id='ads-propaganda> Teste </h1>";
+
+}
+add_filter('the_content', 'add_advertising');
+*/
